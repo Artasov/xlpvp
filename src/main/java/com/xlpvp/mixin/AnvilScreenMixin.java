8e47005598a302437c976510c68af3a83e77fef7
Artasov <ivanhvalevskey@gmail.com>
2026-06-02 12:24:36 +0300

    Make anvil screen mixin compatible with Apothic Enchanting

--- src/main/java/com/xlpvp/mixin/AnvilScreenMixin.java
+++ src/main/java/com/xlpvp/mixin/AnvilScreenMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(AnvilScreen.class)
 public abstract class AnvilScreenMixin {
 
-    @ModifyConstant(method = "renderLabels", constant = @Constant(intValue = 40))
+    @ModifyConstant(method = "renderLabels", constant = @Constant(intValue = 40), require = 0)
     private int xlpvp$showHighAnvilCost(int original) {
         return Integer.MAX_VALUE;
     }
